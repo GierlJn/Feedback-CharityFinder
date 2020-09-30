fa0562458cb20be8bf1e99d8a50fdb967bb09cf9
Julian <julian.gierl@gmx.de>
2020-09-30 11:42:49 +0200

    Added FBButton

--- FeedBack2/SearchVC.swift
+++ FeedBack2/SearchVC.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class SearchVC: UIViewController{
     
+    let actionButton = FBButton(title: "Get Impact", backgroundColor: .systemBlue)
+    
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
+        configureActionButton()
+    }
+    
+    private func configureActionButton(){
+        view.addSubview(actionButton)
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150),
+            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            actionButton.heightAnchor.constraint(equalToConstant: 40),
+            actionButton.widthAnchor.constraint(equalToConstant: 150)
+        
+        ])
     }
 }
