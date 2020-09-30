f430d559ec5b112b7ffcd628a9fef1f1b762395e
Julian <julian.gierl@gmx.de>
2020-09-30 11:22:53 +0200

    Initial TabVC's added

---
+++ FeedBack2/FBTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  FBTabBarController.swift
+//  FeedBack2
+//
+//  Created by Julian Gierl on 30.09.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class FBTabBarController: UITabBarController{
+    
+    override func viewDidLoad() {
+        
+        UITabBar.appearance().tintColor = .systemBlue
+        viewControllers = [createSearchVC(), createFavoritesVC()]
+    }
+    
+    
+    private func createSearchVC() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    private func createFavoritesVC() -> UINavigationController{
+        let favouritesVC = FavouritesVC()
+        favouritesVC.title = "Favourites"
+        favouritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favouritesVC)
+    }
+
+}
---
+++ FeedBack2/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  FeedBack2
+//
+//  Created by Julian Gierl on 30.09.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController{
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+    }
+}
