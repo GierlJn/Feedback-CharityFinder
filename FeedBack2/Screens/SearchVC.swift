b570a02c1e62a07f5b66439e7165646eb2d87de8
Julian <julian.gierl@gmx.de>
2020-10-02 13:10:59 +0200

    Added FBTextField

--- FeedBack2/Screens/SearchVC.swift
+++ FeedBack2/Screens/SearchVC.swift
@@ -11,10 +11,12 @@ import UIKit
 class SearchVC: UIViewController{
     
     let actionButton = FBButton(title: "Get Impact", backgroundColor: .systemBlue)
+    let textfield = FBTextField(placeholder: "Enter amount to donate")
     
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
         configureActionButton()
+        configureTextField()
     }
     
     private func configureActionButton(){
@@ -23,9 +25,19 @@ class SearchVC: UIViewController{
             actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150),
             actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             actionButton.heightAnchor.constraint(equalToConstant: 40),
-            actionButton.widthAnchor.constraint(equalToConstant: 150)
+            actionButton.widthAnchor.constraint(equalToConstant: 200)
         ])
         actionButton.addTarget(self, action: #selector(actionButtonPressed), for: .touchUpInside)
+    }
+    
+    private func configureTextField(){
+        view.addSubview(textfield)
+        NSLayoutConstraint.activate([
+            textfield.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -50),
+            textfield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textfield.heightAnchor.constraint(equalToConstant: 40),
+            textfield.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
     
     @objc func actionButtonPressed(){
---
+++ FeedBack2/Views/FBTextField.swift
@@ -0,0 +1,49 @@
+//
+//  FBTextField.swift
+//  FeedBack2
+//
+//  Created by Julian Gierl on 02.10.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class FBTextField: UITextField{
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(placeholder: String){
+        self.init()
+        self.placeholder = placeholder
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        returnKeyType = .go
+        clearButtonMode = .whileEditing
+        
+       
+    }
+}
