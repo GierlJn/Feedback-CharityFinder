5243bb977a9ca8b63aaa5c77fa03de482151a90b
Julian <julian.gierl@gmx.de>
2020-10-08 08:51:51 +0200

    Addd Colors+Ext

---
+++ FeedBack2/Extensions/Colors+Ext.swift
@@ -0,0 +1,46 @@
+//
+//  Colors.swift
+//  FeedBack2
+//
+//  Created by Julian Gierl on 08.10.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+
+
+extension UIColor {
+    static var pineApplePurple: UIColor { return UIColor(hex: "#413C58")!}
+    static var columbiaBlue: UIColor { return UIColor(hex: "#C4D7F2")!}
+    static var cambridgeBlue: UIColor { return UIColor(hex: "#A3C4BC")!}
+    static var turquoiseGreen: UIColor { return UIColor(hex: "#BFD7B5")!}
+    
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
+
+
