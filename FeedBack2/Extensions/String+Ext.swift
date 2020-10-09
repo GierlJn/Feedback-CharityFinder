3896bfd5f306cef4a305fa6ef175798054108c61
Julian <julian.gierl@gmx.de>
2020-10-09 11:59:42 +0200

    funds to var

--- FeedBack2/Extensions/String+Ext.swift
+++ FeedBack2/Extensions/String+Ext.swift
@@ -10,13 +10,13 @@ import Foundation
 
 extension String{
     
-    var firstUppercased: String { return prefix(1).uppercased() + dropFirst() }
+    var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
     
-    var condensed: String { return replacingOccurrences(of: "[\\s\n]+", with: " ", options: .regularExpression)}
+    var condensed: String { replacingOccurrences(of: "[\\s\n]+", with: " ", options: .regularExpression)}
     
-    func cleanOutputName(with currency: Currency)->String{ self.removePluralParanthesis().replaceCurrencyWording(with: currency).lowercased().firstUppercased.condensed }
+    var withoutParanthesis: String { self.replacingOccurrences(of: "\\([^()]*\\)", with: "", options: .regularExpression) }
     
-    func removePluralParanthesis()->String{ self.replacingOccurrences(of: "\\([^()]*\\)", with: "", options: .regularExpression) }
+    func cleanOutputName(with currency: Currency)->String{ self.withoutParanthesis.replaceCurrencyWording(with: currency).lowercased().firstUppercased.condensed }
     
     func replaceCurrencyWording(with currency: Currency)->String{ return self.replacingOccurrences(of: "pound", with: currency.rawValue) }
 }
