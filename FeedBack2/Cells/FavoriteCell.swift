01dab2a50a0fae5baf541c7bc44e1b8555bbaeee
Julian <julian.gierl@gmx.de>
2020-11-01 11:21:40 +0100

    Favorites get shown

---
+++ FeedBack2/Cells/FavoriteCell.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteCell.swift
+//  FeedBack2
+//
+//  Created by Julian Gierl on 01.11.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell{
+    static let reuseIdentifier = "FavoriteCell"
+    
+    let titleLabel = FBTitleLabel(frame: .zero)
+    let logoImageView = FBLogoImageView(frame: .zero)
+    let padding = 20
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureLogoImageView()
+        configureLabels()
+        backgroundColor = .init(white: 0, alpha: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(charity: Charity){
+        titleLabel.text = charity.name
+        logoImageView.setLogoImage(logoUrl: charity.logoUrl)
+    }
+    
+    private func configureLogoImageView(){
+        contentView.addSubview(logoImageView)
+        logoImageView.setRoundCorners()
+        logoImageView.snp.makeConstraints { (maker) in
+            maker.size.equalTo(100)
+            maker.left.equalTo(padding)
+            maker.centerY.equalTo(snp.centerY)
+        }
+    }
+    
+    private func configureLabels(){
+        contentView.addSubview(titleLabel)
+        //titleLabel.textAlignment = .right
+        titleLabel.snp.makeConstraints { (maker) in
+            maker.top.equalTo(snp.top).offset(padding)
+            maker.left.equalTo(logoImageView.snp.right).offset(15)
+            maker.right.equalTo(-25)
+            maker.height.equalTo(40)
+        }
+        
+    }
+    
+}
+
+
--- FeedBack2/Screens/FavouritesVC.swift
+++ FeedBack2/Screens/FavouritesVC.swift
@@ -10,8 +10,61 @@ import UIKit
 
 class FavouritesVC: UIViewController{
     
+    var charities = [Charity]()
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureTableView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        loadFavorites()
+    }
+    
+    fileprivate func loadFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] (result) in
+            guard let self = self else { return }
+            switch result{
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Error", message: error.errorMessage, buttonTitle: "Ok")
+            case .success(let charities):
+                self.charities = charities
+                self.updateUI()
+            }
+        }
+    }
+    
+    private func configureTableView(){
+        view.addSubview(tableView)
+        tableView.pinToEdges(of: view)
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 80
+        tableView.removeExcessCells()
+        
+    }
+    
+    private func updateUI(){
+        tableView.reloadData()
+    }
+    
+}
+
+
+extension FavouritesVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        charities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseIdentifier) as! FavoriteCell
+        cell.set(charity: charities[indexPath.row])
+        return cell
     }
     
 }
