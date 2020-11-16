e2fcf430da3a67287cdefce122b5875b323fb454
Julian <julian.gierl@gmx.de>
2020-11-16 11:49:12 +0100

    font color in changed

--- FeedBack2/ViewController/OutputCalculationVC.swift
+++ FeedBack2/ViewController/OutputCalculationVC.swift
@@ -92,7 +92,7 @@ class OutputCalculationVC: UIViewController{
             maker.top.equalTo(titleLabel.snp.bottom).offset(8)
             maker.left.equalTo(containerView.snp.left).offset(padding)
             maker.right.equalTo(containerView.snp.right).offset(-padding)
-            maker.height.equalTo(80)
+            maker.height.equalTo(60)
         }
         
         donationTextFieldView.snp.makeConstraints { (maker) in
@@ -131,13 +131,14 @@ class OutputCalculationVC: UIViewController{
     }
     
     fileprivate func configureExitButton(){
+        dismissButten = FBButton(title: "Ok")
         containerView.addSubview(dismissButten)
-        dismissButten.setTitle("Return", for: .normal)
+        dismissButten.addTarget(self, action: #selector(dismissButtonPressed), for: .touchUpInside)
         dismissButten.snp.makeConstraints { (maker) in
             maker.height.equalTo(44)
             maker.top.equalTo(actionContentView.snp.bottom).offset(padding)
-            maker.left.equalTo(containerView.snp.left).offset(padding)
-            maker.right.equalTo(containerView.snp.right).offset(-padding)
+            maker.left.equalTo(containerView.snp.left).offset(padding*2)
+            maker.right.equalTo(containerView.snp.right).offset(-padding*2)
             maker.bottom.equalTo(containerView.snp.bottom).offset(-padding)
         }
     }
@@ -155,6 +156,8 @@ class OutputCalculationVC: UIViewController{
         let formatted = String(format: "%.0f", impact)
         
         messageLabel.text = "\(formatted) \(output.name?.formatOutputName(with: currency, wording: .plural) ?? "")"
+        messageLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        messageLabel.textColor = .label
         messageLabel.numberOfLines = 2
         messageLabel.pinToEdges(of: actionContentView)
     }
@@ -178,7 +181,8 @@ class OutputCalculationVC: UIViewController{
     
     private func showImpact(){
         titleLabel.text = "Your donation may fund"
-
+        titleLabel.textColor = .secondaryLabel
+        titleLabel.font = .preferredFont(forTextStyle: .body)
         donationTextFieldView.removeFromSuperview()
         configureMessageLabel()
         
