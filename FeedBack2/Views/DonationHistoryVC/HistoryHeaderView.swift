01eec8b24377fbe9b2459dba7f9a8c19606c8b9e
Julian <julian.gierl@gmx.de>
2020-11-23 10:24:40 +0100

    refactored to separate class

---
+++ FeedBack2/Views/DonationHistoryVC/HistoryHeaderView.swift
@@ -0,0 +1,76 @@
+//
+//  HistoryHeaderView.swift
+//  FeedBack
+//
+//  Created by Julian Gierl on 23.11.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class HistoryHeaderView: UIView{
+    
+    var titleLabel = FBTitleLabel(textAlignment: .center)
+    var numbersStackView = NumbersStackView()
+    var toggleButton = UIButton()
+    
+    let padding = 20
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        backgroundColor = .systemBackground
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateUI(with donations: [Donation]){
+        numbersStackView.updateUI(with: donations)
+    }
+    
+    private func configure(){
+        configureTitleLabel()
+        configureNumberStackView()
+        configureToggleButton()
+    }
+    
+    private func configureTitleLabel(){
+        addSubview(titleLabel)
+        titleLabel.text = "Your donations"
+        titleLabel.snp.makeConstraints { (maker) in
+            maker.top.equalTo(snp.top)
+            maker.height.equalTo(50)
+            maker.left.equalTo(snp.left).offset(padding)
+            maker.right.equalTo(snp.right).offset(-padding)
+        }
+    }
+    
+    private func configureNumberStackView(){
+        addSubview(numbersStackView)
+        numbersStackView.axis = .vertical
+        numbersStackView.snp.makeConstraints { (maker) in
+            maker.top.equalTo(titleLabel.snp.bottom)
+            maker.height.equalTo(50)
+            maker.left.lessThanOrEqualTo(snp.left).offset(padding)
+            maker.right.lessThanOrEqualTo(snp.right).offset(-padding)
+            maker.centerX.equalTo(snp.centerX)
+        }
+    
+    }
+    
+    private func configureToggleButton(){
+        addSubview(toggleButton)
+        toggleButton.setTitle("Toggle View", for: .normal)
+        
+        toggleButton.snp.makeConstraints { (maker) in
+            maker.top.equalTo(numbersStackView.snp.bottom)
+            maker.left.equalTo(snp.left).offset(padding)
+            maker.right.equalTo(snp.right).offset(-padding)
+            maker.bottom.equalTo(snp.bottom)
+        }
+        
+        toggleButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
+    }
+}
---
+++ FeedBack2/Views/DonationHistoryVC/NumbersStackView.swift
@@ -0,0 +1,46 @@
+//
+//  NumbersStackView.swift
+//  FeedBack
+//
+//  Created by Julian Gierl on 23.11.20.
+//  Copyright © 2020 Julian Gierl. All rights reserved.
+//
+
+import UIKit
+
+class NumbersStackView: UIStackView{
+    
+    let totalLabelView = FBTitleLabel(textAlignment: .center)
+    let totalDonationsSumLabel = FBSubTitleLabel(textAlignment: .center)
+    
+    let currency = PersistenceManager.retrieveCurrency()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateUI(with donations: [Donation]){
+        totalLabelView.text = String(donations.count)
+        
+        let donationsSumForDollar = donations.reduce(0.0, { (result, donation) -> Float in
+            let amount = donation.amount
+            let amountInPound = amount * donation.currency.relativeValueToPound
+            let amountInSelectedCurrency = amountInPound / currency.relativeValueToPound
+
+            return result + amountInSelectedCurrency
+        })
+        
+        totalDonationsSumLabel.text = String(Int(donationsSumForDollar)) + currency.symbol
+        
+    }
+    private func configure(){
+        totalLabelView.textColor = .outputColor
+        self.addArrangedSubview(totalLabelView)
+        self.addArrangedSubview(totalDonationsSumLabel)
+    }
+}
