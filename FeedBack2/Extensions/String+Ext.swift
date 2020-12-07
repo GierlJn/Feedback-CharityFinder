82e05157c9fcb589f9d03afd8b7e7832e21223b2
Julian <julian.gierl@gmx.de>
2020-12-07 10:44:30 +0100

    refactored marks

--- FeedBack2/Extensions/String+Ext.swift
+++ FeedBack2/Extensions/String+Ext.swift
@@ -10,6 +10,8 @@ import UIKit
 
 extension String{
     
+    //MARK: Text formatting
+    
     var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
     
     var condensed: String { replacingOccurrences(of: "[\\s\n]+", with: " ", options: .regularExpression) }
@@ -30,57 +32,40 @@ extension String{
     }
     
     func getSingularWording()->String{
-        let fullString = self
-        guard let parenthesisRange = fullString.range(of: #"(?<=\()(.*?)(?=\))"#, options: .regularExpression) else {
-            return self
-        }
-        
-        var paranthesisContent = fullString[parenthesisRange]
+        guard let parenthesisRange = self.range(of: #"(?<=\()(.*?)(?=\))"#, options: .regularExpression) else { return self }
+        var paranthesisContent = self[parenthesisRange]
         
         if(paranthesisContent.hasPrefix("singular: ")){
             paranthesisContent.removeFirst("singular: ".count)
-        }else{
-            return self
-        }
+        }else{ return self }
         
-        let withoutParenthesis = fullString.withoutParanthesis.condensed
-        
-        var array = withoutParenthesis.components(separatedBy: " ")
+        var array = self.withoutParanthesis.condensed.components(separatedBy: " ")
         array.removeFirst()
-        
-        let result = String(paranthesisContent) + " " + String(array.joined(separator: " "))
-        return result
+        return "\(paranthesisContent) \(array.joined(separator: " "))"
     }
     
     func getPluralWording()->String{
-        
-        var fullString = self
-        
-        guard let parenthesisRange = fullString.range(of: #"(?<=\()(.*?)(?=\))"#, options: .regularExpression) else { return self }
-        
-        var paranthesisContent = fullString[parenthesisRange]
+        var string = self
+        guard let parenthesisRange = string.range(of: #"(?<=\()(.*?)(?=\))"#, options: .regularExpression) else { return self }
+        var paranthesisContent = string[parenthesisRange]
         
         if(paranthesisContent.hasPrefix("plural: ")){
             paranthesisContent.removeFirst("plural: ".count)
         }else if(paranthesisContent == "s"){
-            fullString = fullString.replacingOccurrences(of: "(", with: "")
-            fullString = fullString.replacingOccurrences(of: ")", with: "")
-            return fullString
-        }
-        else{
-            return self
-        }
+            string = string.replacingOccurrences(of: "(", with: "")
+            string = string.replacingOccurrences(of: ")", with: "")
+            return string
+        }else{ return self }
         
-        let withoutParenthesis = fullString.withoutParanthesis.condensed
-        
-        var array = withoutParenthesis.components(separatedBy: " ")
+        var array = string.withoutParanthesis.condensed.components(separatedBy: " ")
         array.removeFirst()
         
-        let result = String(paranthesisContent) + " " + String(array.joined(separator: " "))
-        return result
+        return "\(paranthesisContent) \(array.joined(separator: " "))"
     }
     
-    func replaceCurrencyWording(with currency: Currency)->String{ return self.replacingOccurrences(of: "pound", with: currency.rawValue) }
+    func replaceCurrencyWording(with currency: Currency)->String{ return self.replacingOccurrences(of: Currency.pound.rawValue, with: currency.rawValue) }
+    
+    //MARK: Utilities
     
     func height(withWidth width: CGFloat, font: UIFont) -> CGFloat {
         let maxSize = CGSize(width: width, height: CGFloat.greatestFiniteMagnitude)
